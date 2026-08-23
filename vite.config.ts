46eb56c79535f81c997659971af6cdf8b8411b3c
KARISHMA R <karishmaramesh16@gmail.com>
2026-08-23 18:32:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@/contexts': fileURLToPath(new URL('./', import.meta.url)),
+      '@/components': fileURLToPath(new URL('./', import.meta.url)),
+      '@/layouts': fileURLToPath(new URL('./', import.meta.url)),
+      '@/pages': fileURLToPath(new URL('./', import.meta.url)),
+      '@/services': fileURLToPath(new URL('./', import.meta.url)),
+      '@/types': fileURLToPath(new URL('./index (2).ts', import.meta.url)),
+      '@': fileURLToPath(new URL('./', import.meta.url)),
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
